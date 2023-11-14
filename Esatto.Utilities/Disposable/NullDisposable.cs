27d780c415678661a47056063d612bb3e11aea0f
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2023-11-14 10:57:53 -0600

    Added NullDisposable

---
+++ Esatto.Utilities/Disposable/NullDisposable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esatto.Utilities
+{
+    public sealed class NullDisposable : IDisposable
+    {
+        public static IDisposable Instance { get; } = new NullDisposable();
+        public void Dispose()
+        {
+            // nop
+        }
+    }
+}
