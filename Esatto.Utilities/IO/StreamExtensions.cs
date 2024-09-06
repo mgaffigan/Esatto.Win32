bc1a3f7aa18084804653cc7c476e1b2eb634a8ef
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2024-09-06 15:23:31 -0500

    Bugfix for exception on GetByteArray

--- Esatto.Utilities/IO/StreamExtensions.cs
+++ Esatto.Utilities/IO/StreamExtensions.cs
@@ -1,24 +1,26 @@
-﻿namespace Esatto.Utilities
-{
-    public static class StreamExtensions
-    {
-        public static byte[] GetByteArray(this Stream source)
-        {
-            var msSource = source as MemoryStream;
-            if (msSource == null)
-            {
-                // network stream does not support seek
-                // therefore cannot init with capacity
-                msSource = source is FileStream fs ? new MemoryStream(checked((int)fs.Length)) : new MemoryStream();
-                source.CopyTo(msSource);
-            }
+﻿namespace Esatto.Utilities;
 
-            var data = msSource.GetBuffer();
-            if (data.Length != msSource.Length)
-            {
-                Array.Resize(ref data, checked((int)msSource.Length));
-            }
-            return data;
-        }
+public static class StreamExtensions
+{
+    public static byte[] GetByteArray(this Stream source)
+    {
+        if (source is MemoryStream ms) return ms.ToArray();
+
+        // network stream does not support seek
+        // therefore cannot init with capacity
+        using var msSource = source is FileStream fs ? new MemoryStream(checked((int)fs.Length)) : new MemoryStream();
+        source.CopyTo(msSource);
+        return msSource.ToArray();
+    }
+
+    public static async Task<byte[]> GetByteArrayAsync(this Stream source)
+    {
+        if (source is MemoryStream ms) return ms.ToArray();
+
+        // network stream does not support seek
+        // therefore cannot init with capacity
+        using var msSource = source is FileStream fs ? new MemoryStream(checked((int)fs.Length)) : new MemoryStream();
+        await source.CopyToAsync(msSource);
+        return msSource.ToArray();
     }
 }
