f9bb1024aa708200c8d181921566bbcbc0e9d923
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2025-06-20 19:24:56 -0500

    3.0.21

---
+++ HVSocket/Esatto.Win32.HVSocket.KestrelListener/Win32/HyperVSocketEndPoint.cs
@@ -0,0 +1,56 @@
+﻿using static Esatto.Win32.HVSocket.NativeMethods;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Esatto.Win32.HVSocket;
+
+[Serializable]
+internal sealed class HyperVSocketEndPoint : EndPoint
+{
+    public HyperVSocketEndPoint(Guid vmId, Guid serviceId)
+    {
+        this.VmId = vmId;
+        this.ServiceId = serviceId;
+    }
+
+    public override AddressFamily AddressFamily => AF_HYPERV;
+
+    public Guid VmId { get; set; }
+
+    public Guid ServiceId { get; set; }
+
+    public override EndPoint Create(SocketAddress sockAddr)
+    {
+        if (sockAddr == null ||
+            sockAddr.Family != AF_HYPERV ||
+            sockAddr.Size != HYPERV_SOCK_ADDR_SIZE)
+        {
+            throw new ArgumentException("Invalid socket address", nameof(sockAddr));
+        }
+
+        var buffer = sockAddr.Buffer.Span;
+        var vmId = new Guid(buffer.Slice(4, 16));
+        var serviceId = new Guid(buffer.Slice(20, 16));
+        return new HyperVSocketEndPoint(vmId, serviceId);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is HyperVSocketEndPoint endpoint
+            && this.VmId == endpoint.VmId && this.ServiceId == endpoint.ServiceId;
+    }
+
+    public override int GetHashCode() => ServiceId.GetHashCode() ^ VmId.GetHashCode();
+
+    public override SocketAddress Serialize()
+    {
+        var sockAddress = new SocketAddress(AF_HYPERV, HYPERV_SOCK_ADDR_SIZE);
+        var buffer = sockAddress.Buffer.Span;
+        buffer[2] = 0;
+        _ = VmId.TryWriteBytes(buffer.Slice(4, 16));
+        _ = ServiceId.TryWriteBytes(buffer.Slice(20, 16));
+        return sockAddress;
+    }
+
+    public override string ToString() => $"{VmId:n}.{ServiceId:n}";
+}
---
+++ HVSocket/Esatto.Win32.HVSocket.KestrelListener/Win32/NativeMethods.cs
@@ -0,0 +1,10 @@
+﻿using System.Net.Sockets;
+
+namespace Esatto.Win32.HVSocket;
+
+internal static class NativeMethods
+{
+    public const ProtocolType HV_PROTOCOL_RAW = (ProtocolType)1;
+    public const AddressFamily AF_HYPERV = (AddressFamily)34;
+    public const int HYPERV_SOCK_ADDR_SIZE = 36;
+}
