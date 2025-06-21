3cf8079be1f85416bebc418475b4d8be21d91773
Mitch Gaffigan <mitch.gaffigan@comcast.net>
2025-06-20 19:16:53 -0500

    Added sample of Window Location Monitor

---
+++ WindowLocationMonitorTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using Esatto.Win32.Windows;
+using System.Windows;
+
+var hwnd = IntPtr.Parse(Console.ReadLine()!, System.Globalization.NumberStyles.HexNumber);
+using var monitor = new AbsoluteWindowLocationMonitor(new Win32Window(hwnd));
+monitor.WindowMoved += (s, e) => Console.WriteLine(e.WindowRect);
+monitor.DragBegin += (s, e) => Console.WriteLine("DragBegin");
+monitor.DragEnd += (s, e) => Console.WriteLine("DragEnd");
+monitor.Exception += (s, e) => Console.WriteLine(e.ExceptionObject);
+
+var a = new Application();
+a.Run();
